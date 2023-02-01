2bb16c488b599b2017f5758b98b06880d3c1635f
Anto Sergeevich <Fokkuszim08@mail.ru>
2023-02-01 18:31:14 +0300

    Domashka

---
+++ DZ № 1.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
