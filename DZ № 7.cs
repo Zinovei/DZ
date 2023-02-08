4c5be438440f4a622bf047ea798dd2df2e8a3c34
Anton Sergeevich <Fokkuszim08@mail.ru>
2023-02-08 09:39:22 +0300

    DZ__7

---
+++ DZ № 7.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+Console.WriteLine("Ведите значение от 1 до 7: ");
+int n = int.Parse(Console.ReadLine()!);
+if(n == 6 || n == 7)
+    {
+        Console.WriteLine("Да");
+    }
+else if (n <1 || n > 7)
+    {
+        Console.WriteLine("Число не распознано");
+    }
+else
+{
+    Console.WriteLine("Нет");
+}
